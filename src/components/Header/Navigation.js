697ef3d51b88b2a5e83c17acb27b2807f360977c
Md Ishaq Ibrahim <mdishaqibrohim@gmail.com>
2022-04-11 12:48:54 +0600

    header nav created

---
+++ src/components/Header/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Navbar, NavbarBrand} from 'reactstrap';
+const Navigation = () => {
+  return (
+    <div>
+      <Navbar dark color='dark' className=''>
+        <div className="row justify-content-start">
+        <NavbarBrand href='/'>Bohubrihi Restaurent</NavbarBrand>
+        </div>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Navigation;
